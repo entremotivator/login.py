a1e7254278df1dd938299fa823e9b196fe71e04e
entremotivator <104661532+entremotivator@users.noreply.github.com>
2024-08-26 20:50:05 -0400

    Delete pages/page4.py

--- pages/page4.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def render():
-    """Render page 4 with unique content."""
-    st.title("Page 4")
-    st.write("This is the content for page 4.")
-    # Additional content for page 4 can be added here.

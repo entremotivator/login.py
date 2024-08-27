ae286d3bd0e5aa8976ca70043b0f472006406ae2
entremotivator <104661532+entremotivator@users.noreply.github.com>
2024-08-26 20:40:18 -0400

    Create main.py

---
+++ pages/main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def render():
+    """Render the main page after user login."""
+    st.title("Welcome to the Application")
+    st.write("This is the main content of the application.")
+    # Additional main page content can be added here.
